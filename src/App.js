7bfeea359d3e3c0eeca24aaadf0ae24761ef488a
Kittipong2001 <128574773+Kittipong2001@users.noreply.github.com>
2023-05-08 02:02:15 +0700

    Add header section

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
-import "./App.css";
+import "./style/App.css";
+import General from "./components/General.js";
 
 function App() {
-	return <div className="App"></div>;
+	return (
+		<div className="App">
+			<div className="header">
+				<h1>CV Maker</h1>
+			</div>
+			<div className="content"></div>
+		</div>
+	);
 }
-
 export default App;
